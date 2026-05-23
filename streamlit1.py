c69d91843adba81f766d5551792fb59eb94d8eee
Maria <m67074@gmail.com>
2026-05-23 15:38:32 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -58,13 +58,13 @@ st.title('Book price prediction')
 st.subheader('Enter book information:')
 
 title = st.text_input('Book title')
-author = st.text_input('Book author')
+Author = st.text_input('Book author')
 edition  =  st.text_input('Book edition ') 
 genre = st.text_input('Book genre')
 reviews = st.number_input('Book reviews', min_value=0.0, value=4.4, step=0.1)
 ratings = st.number_input('Book ratings',step=1)
-# Synopsis  =  st.text_input('Book synopsis ')   
-# BookCategory  =  st.text_input('Book category ') 
+Synopsis  =  st.text_input('Book synopsis ')   
+BookCategory  =  st.text_input('Book category ') 
 
 if st.button('Прогнозувати ціну'):
     input_data = pd.DataFrame({
