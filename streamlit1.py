632227afef0f695aaf096f4999bc593cba823475
Maria <m67074@gmail.com>
2026-05-23 16:27:48 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -111,7 +111,6 @@ if st.button('Прогнозувати ціну'):
     #    X_predict_input.loc[:, col] = X_predict_input.loc[:, col].fillna(col_mean_input if not pd.isna(col_mean_input) else 0)
     #X_predict_scaled_numerical = feature_scaler.transform(X_predict_input[numerical_features])
     #X_predict_combined = np.hstack((X_predict_scaled_numerical, X_predict_input[categorical_features].values))
-    #linear_pred = model.predict(X_predict_combined)[0]
     
 
     st.subheader('Прогнозовані ціни:')
