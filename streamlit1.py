31c295b8da55609e269daa04d5c3abbcdcf0e3d6
Maria <m67074@gmail.com>
2026-05-23 15:54:07 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -86,7 +86,7 @@ if st.button('Прогнозувати ціну'):
 
     X_predict_combined = np.hstack((X_predict_scaled_numerical, X_predict_input[categorical_features].values))
 
-    linear_pred = linear_model.predict(X_predict_combined)[0]
+    linear_pred = model.predict(X_predict_combined)[0]
 
     st.subheader('Прогнозовані ціни:')
     st.metric(label="Linear Regression", value=f"{linear_pred:.2f} ")
