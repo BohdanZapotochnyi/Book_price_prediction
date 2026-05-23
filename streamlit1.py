324ac9a08e7a5cce1ffd64ce6c706654609f4424
Maria <m67074@gmail.com>
2026-05-23 16:30:00 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -104,7 +104,7 @@ if st.button('Прогнозувати ціну'):
     for col in ['Reviews', 'Ratings']:
         col_mean_input = X_predict_input[col].mean() # Should be just the input value, but for consistency
         X_predict_input.loc[:, col] = X_predict_input.loc[:, col].fillna(col_mean_input if not pd.isna(col_mean_input) else 0)
-    X_predict_scaled_numerical = feature_scaler.transform(X_predict_input[numerical_features])
+    X_predict_scaled_numerical = scaler.transform(X_predict_input[numerical_features])
     X_predict_combined = np.hstack((X_predict_scaled_numerical, X_predict_input[categorical_features].values))
 
     
