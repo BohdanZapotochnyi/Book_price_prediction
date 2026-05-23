c9ab81cbeda6379b7080340ca1d8d48efdd1481a
Maria <m67074@gmail.com>
2026-05-23 15:08:01 +0300

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 from sklearn.linear_model import LinearRegression, ElasticNet, Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
